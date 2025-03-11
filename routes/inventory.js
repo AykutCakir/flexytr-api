96f17d58b00b6b3a5f8b2f3ef0405c24fcbcccd9
AykutCakir <154988936+AykutCakir@users.noreply.github.com>
2025-03-11 13:39:44 +0300

    Update inventory.js

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Inventory } = require('../db');
+const { Inventory } = require('../models');
 
 // Tüm envanter ürünlerini getir
 router.get('/', async (req, res) => {
