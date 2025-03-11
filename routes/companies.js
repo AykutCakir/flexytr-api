69603b885035d250c6c8b2174d676986282ade45
AykutCakir <154988936+AykutCakir@users.noreply.github.com>
2025-03-11 13:27:01 +0300

    Update companies.js

--- routes/companies.js
+++ routes/companies.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Company } = require('../db');
+const { Company } = require('../models');
 
 // Tüm firmaları getir
 router.get('/', async (req, res) => {
