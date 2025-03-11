5d50912ba1ba4a5efa00899396be1af874c6319b
AykutCakir <154988936+AykutCakir@users.noreply.github.com>
2025-03-11 13:51:13 +0300

    Update TaskHistory.js

--- models/TaskHistory.js
+++ models/TaskHistory.js
@@ -1,37 +1,44 @@
 const { DataTypes } = require('sequelize');
 
-module.exports = (sequelize) => sequelize.define('TaskHistory', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  taskId: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  action: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  user: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  creatorName: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  previousValue: {
-    type: DataTypes.TEXT,
-    allowNull: true
-  },
-  newValue: {
-    type: DataTypes.TEXT,
-    allowNull: true
-  }
-}, {
-  tableName: 'TaskHistory',
-  timestamps: true,
-  underscored: false
-}); 
+module.exports = (sequelize) => {
+  const TaskHistory = sequelize.define('TaskHistory', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    taskId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Tasks',
+        key: 'id'
+      }
+    },
+    action: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    user: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    creatorName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    previousValue: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    newValue: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    }
+  }, {
+    tableName: 'TaskHistories',
+    timestamps: true
+  });
+
+  return TaskHistory;
+}; 
