8e24a7fb805181104a8640e88ee3eed8b686461c
AykutCakir <154988936+AykutCakir@users.noreply.github.com>
2025-03-11 13:50:45 +0300

    Update index.js

--- models/index.js
+++ models/index.js
@@ -37,15 +37,31 @@ try {
   const Report = require('./Report')(sequelize);
   const User = require('./User')(sequelize);
   const Inventory = require('./Inventory')(sequelize);
+  const Task = require('./Task')(sequelize);
+  const TaskHistory = require('./TaskHistory')(sequelize);
+  const Call = require('./Call')(sequelize);
 
   // İlişkileri tanımla
   Report.belongsTo(User, { foreignKey: 'userId' });
   User.hasMany(Report, { foreignKey: 'userId' });
 
+  Task.belongsTo(User, { as: 'assignedUser', foreignKey: 'assignedUserId' });
+  Task.belongsTo(User, { as: 'creator', foreignKey: 'creatorId' });
+  User.hasMany(Task, { as: 'assignedTasks', foreignKey: 'assignedUserId' });
+  User.hasMany(Task, { as: 'createdTasks', foreignKey: 'creatorId' });
+  Task.hasMany(TaskHistory, { foreignKey: 'taskId' });
+  TaskHistory.belongsTo(Task, { foreignKey: 'taskId' });
+
+  Call.belongsTo(User, { foreignKey: 'userId' });
+  User.hasMany(Call, { foreignKey: 'userId' });
+
   // Modelleri dışa aktar
   db.Report = Report;
   db.User = User;
   db.Inventory = Inventory;
+  db.Task = Task;
+  db.TaskHistory = TaskHistory;
+  db.Call = Call;
   db.sequelize = sequelize;
   db.Sequelize = Sequelize;
 
